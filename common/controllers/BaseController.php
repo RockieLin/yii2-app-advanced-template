ae62de910f700a7563b4eb55283491e07a787686
Rockie Lin <rockie.lin.tw@gmail.com>
2024-10-30 11:17:24 +0800

    Update BaseController.php

--- common/controllers/BaseController.php
+++ common/controllers/BaseController.php
@@ -10,22 +10,11 @@ class BaseController extends Controller {
     public function init() {
         parent::init();
 
-        if (Yii::$app->language == "zh-CN") {
-            //簡中轉換
-            Yii::$app->response->on(\yii\web\Response::EVENT_BEFORE_SEND, function (\yii\base\Event $Event) {
-                $response = $Event->sender;
-                if ($response->format === \yii\web\Response::FORMAT_HTML) {
-                    if (!empty($response->data)) {
-                        $response->data = Yii::$app->tool->twTocn($response->data);
-                    }
+        Yii::$app->formatter->nullDisplay = '-';
 
-                    if (!empty($response->content)) {
-                        $response->content = Yii::$app->tool->twTocn($response->content);
-                    }
-
-                    $response->headers->set('Cache-Control', 'no-transform');
-                }
-            });
+        if (isset($_SERVER['HTTP_SEC_PURPOSE']) && $_SERVER['HTTP_SEC_PURPOSE'] === 'prefetch') {
+            echo "no prefetch";
+            exit;
         }
     }
 }
